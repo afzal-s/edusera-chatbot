9a97180368ccc228c46fe9e74fc5c370d1d7dc63
Shaikh Afzal <xfzxlshxikh@gmail.com>
2021-08-01 05:33:01 +0530

    Reorganise the boilerplate and cleanup.

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Edusera</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, assumenda deserunt! Quibusdam amet incidunt reprehenderit enim nihil excepturi quo? Corporis totam reiciendis, quis odit rem minima enim explicabo consequuntur quaerat?</p>
+      </div>
+    )
+  }
+}
+
+export default App;
